30ca2cab8452a9ea8534332160e886d44f4099a8
이재문 <990914s@gmail.com>
2025-09-28 23:15:16 +0900

    터치 이펙트 오류 해결, 사운드 오류 해결

--- Assets/Scripts/TouchScripts/touchEffect.cs
+++ Assets/Scripts/TouchScripts/touchEffect.cs
@@ -14,12 +14,15 @@ public class touchEffect : MonoBehaviour
 
 #if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0))
-           touchPos = Input.mousePosition;
-#else
+        {
+            touchPos = Input.mousePosition;
+        }
+#elif UNITY_IOS || UNITY_ANDROID
         if (Input.touchCount > 0)
+        {
             touchPos = Input.GetTouch(0).position;
+        }
 #endif
-
         if (touchPos != Vector3.zero)
         {
             touchPos.z = distanceFromCamera;
@@ -28,34 +31,39 @@ public class touchEffect : MonoBehaviour
             Quaternion rotation = Camera.main.transform.rotation;
 
             if (touchEffectPrefab != null)
+            {
                 Instantiate(touchEffectPrefab, worldPos, rotation);
+            }
 
             if (audioClip != null && audioSource != null)
+            {
                 audioSource.PlayOneShot(audioClip);
+            }
         }
+            
+
+        // {
+        //     if (Input.GetMouseButtonDown(0))
+        //     Debug.Log("마우스 클릭 감지");
+        //     {
+        //         Debug.Log("터치 감지");
+
+        //         Vector3 touchPos = Input.mousePosition;
+        //         touchPos.z = distanceFromCamera;
+        //         Vector3 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
+
+        //         Instantiate(touchEffectPrefab, worldPos, Camera.main.transform.rotation);
+
+        //         //Vector3(323.976807,45.7141571,1.05563265e-06) 카메라/터치 로테이션값
+        //         Debug.Log("SpawnPos: " + worldPos);
+
+        //         if (audioClip != null)
+        //             audioSource.PlayOneShot(audioClip);
+
+
+        //     }
+        // }
+        
+
     }
-
-    /*{
-        if (Input.GetMouseButtonDown(0))
-        Debug.Log("마우스 클릭 감지");
-        {
-            Debug.Log("터치 감지");
-
-            Vector3 touchPos = Input.mousePosition;
-            touchPos.z = distanceFromCamera;
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(touchPos);
-
-            Instantiate(touchEffectPrefab, worldPos, Camera.main.transform.rotation);
-
-            //Vector3(323.976807,45.7141571,1.05563265e-06) 카메라/터치 로테이션값
-            Debug.Log("SpawnPos: " + worldPos);
-
-            if (audioClip != null)
-                audioSource.PlayOneShot(audioClip);
-
-
-        }
-    }
-    */
-
 }
